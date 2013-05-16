c36bf249754f58adfb665caba3481aa908e1a8a3
Daan van Berkel <daan.v.berkel.1980@gmail.com>
2013-05-16 20:24:04 +0200

    parameterize PointSpec

--- spec/PointSpec.js
+++ spec/PointSpec.js
@@ -17,13 +17,17 @@ describe('Point', function(){
 	expect(p.equals(q)).toBeTruthy();
     });
 
-    it('should know when it is NOT equal to an other point', function(){
-	var p = new kyle.Point(0, 0);
-	var q1 = new kyle.Point(1, 0);
-	var q2 = new kyle.Point(0, 1);
-
-	expect(p.equals(q1)).toBeFalsy();
-	expect(p.equals(q2)).toBeFalsy();
-    });
-
+    describe("(0, 0) not equals", function(){
+	var factory = function(q){
+	    var p = new kyle.Point(0,0);
+	    return function(){ expect(p.equals(q)).toBeFalsy(); }
+	};
+	var name = function(q){
+	    return "to (" + q.x() + "," + q.y() + ")";
+	}
+	var points = [new kyle.Point(1,0), new kyle.Point(0,1)];
+	for (var index = 0; index < points.length; index++) {
+	    it (name(points[index]), factory(points[index]));
+	}
+    })
 });
